0788139409035a1c30ed9aeed553444c189626f5
Your Name <NadVMas@gmail.com>
2017-05-26 20:15:03 +0300

    мой первый проект

---
+++ main.js
@@ -0,0 +1,20 @@
+var value ="2545";
+var result=0;
+var text = 'gvgdhdjfhdkjshrhdkkjl/n hgfduhfishfashhfa'
+result = 2 + 2 * 2;
+
+
+
+
+console.log(result);
+
+
+//обьекты для взаимодействия с браузером и их методы
+document.write(text+'<ghjj>');
+document.write(result);
+
+
+console.log(document.getElementById("1"));
+
+
+undefined
